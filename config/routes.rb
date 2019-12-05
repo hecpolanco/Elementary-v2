be6a22b7bde25c712d119cf78c64a245ac1dddd1
hdp2112 <54225959+hdp2112@users.noreply.github.com>
2019-12-05 10:04:25 -0500

    Revert "Added google sign in functionality"

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'login', to: redirect('/auth/google_oauth2'), as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'home', to: 'home#show'
-  get 'me', to: 'me#show', as: 'me'
-
   get	'/watsons/new',	 to: 'watsons#new', as: 'new_watsons'
   get '/watsons', to: 'watsons#index', as: 'watsons'
   post '/watsons',	to: 'watsons#create'
